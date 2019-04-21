2dd0e8b8328ca4d87e4b881e59682956026f2fc9
Krishnakumar Thangavel <46522737+YazhiRider@users.noreply.github.com>
2019-04-21 06:29:56 +0530

    IoT based Hospital system
    This is a IoT based hospital system, i upload data to the cloud and the doctor can monitor the patients anywhere.if the pulse goes down it starts alarming.

---
+++ secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "YazhiRider"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "password"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 761313			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "827VF7Z7J0DH03AG"   // replace XYZ with your channel write API Key
